03456b14ab07313381bbca0eb0b73056f5f12645
bingyinh <bingyin.hu@gmail.com>
2021-08-20 16:39:04 -0400

    updated to use the semi-auto config generation pipeline

--- spectraHeaderParser.py
+++ spectraHeaderParser.py
@@ -1,5 +1,5 @@
 # skeleton of the big parser class
-import csv
+import json
 from lxml import etree
 from standardizer import standardizer
 import re
@@ -8,7 +8,7 @@ class spectraHeaderParser(object):
     # init function
     def __init__(self):
         # initialize subclasses
-        self.standardizers = self.__loadSC__()
+        self.__loadSC__()
         pass
 
     # header separator function (TODO)
@@ -31,265 +31,107 @@ class spectraHeaderParser(object):
         :returns: a dictionary {'xName': {subclass1, subclass2,...}, 'xUnit': {subclass2}, 'yName': {subclass3}, 'yUnit': {subclass4}}
         :rtype: dict
         '''
-        # predefined xpath-subclass pairs, fill in the rest (TODO)
-        xpathSubclassPairs = {
-            'PROPERTIES/Viscoelastic/DynamicProperties/DynamicPropertyProfile': {
-                'xName': {
-                    self.frequency,
-                    self.temperature,
-                    self.strain
-                },
-                'xUnit': {
-                    self.frequencyUnit,
-                    self.temperatureUnit,
-                    self.strainUnit,
-                    self.dimensionless
-                },
-                'yName': {
-                    self.storageModulus,
-                    self.lossModulus,
-                    self.tanDelta,
-                    self.normalizedStorageModulus,
-                    self.normalizedLossModulus,
-                    self.normalizedTanDelta,
-                    self.shearStorageModulus,
-                    self.shearLossModulus
-                },
-                'yUnit': {
-                    self.modulusUnit,
-                    self.dimensionless
-                }
-            }
-        }
+
+        # remove '/data' and '/Data' from xpath, that's a schema thing
+        xpath = xpath.replace('/data','').replace('/Data','')
         # remove brackets from xpath
         noBracketXpath = re.sub(r'\[.*?\]','',xpath)
-        return xpathSubclassPairs[noBracketXpath]
+        return self.xpathSubclassPairs[noBracketXpath]
 
     # preload subclasses
-    def __loadSC__(self):
+    def __loadSC__(self, config='./data_preparation/nanomineParserConfig.json'):
         '''
         Load standard/non-standard name pairs and load subclasses with those pairs
         '''
-        ## subclass section
-        # For DynamicPropertyProfiles, the following subclasses are needed:
-        #     ----------------------
-        #     Storage Modulus
-        #     Loss Modulus
-        #     Tan Delta
-        #     Normalized Storage Modulus
-        #     Normalized Loss Modulus
-        #     Normalized Tan Delta
-        #     Shear Storage Modulus
-        #     Shear Loss Modulus
-        #     ----------------------
-        #     Temperature
-        #     Frequency (including Angular Frequency)
-        #     Strain
-        #     ----------------------
-        #     TemperatureUnit (Celsius, Kelvin)
-        #     FrequencyUnit (Hz, rad/s)
-        #     StrainUnit (%?)
-        #     ModulusUnit (Pa, MPa, GPa...)
-        
-        # these name pairs should be read from an external file in the future (something like your pathReaderFunc.py), fill in for the rest of the needed subclasses (TODO)
-        namePairs = {
-            'Storage Modulus': {
-                "E'",
-                'Normalized Storage Modulus',
-                "Storage Modulus, E'",
-                "Storage Modulus E'",
-                'Storage Modulus',
-                "Log E'",
-                'Log storage modulus',
-                'Storage modulus',
-                'Storage Modulu',
-                'Log-storage modulus',
-                'Storage Modulus, E',
-                'storage modulus'
-            },
-            'Loss Modulus': {
-                'E"',
-                "E''",
-                'loss modulus',
-                'Loss modulu',
-                'e"',
-                'Loss modulus',
-                'Loss Modulus',
-                "Log(loss factor e'')"
-            },
-            'Tan Delta': {
-                'loss tangent',
-                'Bending Tan delta',
-                'Dielectric Loss Tangent',
-                'Dielctric Loss Factor',
-                'Dielectric loss',
-                'Tan d',
-                'Tan del',
-                'Damping Factor',
-                'Tan δ',
-                'Dissipation Factor',
-                'tangent delta',
-                'Tan delta',
-                'Log Tan delta',
-                'loss factor',
-                'Loss factor',
-                'Damping factor',
-                'Damping Coefficient',
-                'Loss Tangent',
-                'tan del',
-                'dielectric loss tangent',
-                'Permittivity',
-                'Effective dielectric constant',
-                'tan delta',
-                'Tan',
-                'dissipation factor',
-                'Tangent Delta',
-                'Tan Delta',
-                'loss tanget',
-                'tanδ',
-                'Loss tangent',
-                'Dissipation factor',
-                'Dielectric Loss',
-                'dielectric loss',
-                'dielectric loss, tangent',
-                'Dielectric Loss Factor',
-                'Effective dielectric loss'
-            },
-            'Normalized Storage Modulus': {
-                "E'/E'max"
-            },
-            'Normalized Loss Modulus': {
-                'E"/E"max'
-            },
-            'Normalized Tan Delta': {
-                'normalized tan delta'
-            },
-            'Shear Storage Modulus': {
-                'Shear Storage Modulus',
-                "Storage Modulus, G'",
-                'shear storage modulus',
-                "G'",
-                'Shear storage modulus G'
-            },
-            'Shear Loss Modulus': {
-                "Loss Modulus, G''",
-                'Shear Loss Modulus',
-                "G''",
-                'G"'
-            },
-            'Temperature': {
-                'Temperatur',
-                'Temperature',
-                'Temp',
-                'T',
-                'Tempeature',
-                'temperature'
-            },
-            'Frequency': {
-                'Fequency',
-                'f',
-                'Frequency',
-                'Frequecy',
-                'Log f',
-                'Angular velocity',
-                'freq/Hz',
-                'Oscillation frequency, ω',
-                'Angular Frequency',
-                'w',
-                'Frequenc',
-                'Freq',
-                'Angular frequency',
-                'freq',
-                'ang. frequency',
-                'frequency',
-                'Angular Velocity'
-            },
-            'Strain': {
-                'strain',
-                'strain rate',
-                'Critical strain',
-                'Hencky strain',
-                'Static Strain',
-                'Failure Strain',
-                'strain to failure'
-            },
-            'Celsius': {
-                '*C',
-                '°c',
-                'c',
-                'c*',
-                'cel',
-                'cel.',
-                'celsius',
-                'deg C',
-                'Deg. Celcius',
-                '°C'
-            },
-            'Kelvin': {
-                'K',
-                'kelvin'
-            },
-            'Hz': {
-                'Hertz',
-                'Hz'
-            },
-            'kHz': {
-                '1000 Hz',
-                '*10^3 Hz'
-            },
-            'MHz': {
-                '1000000 Hz'
-                '*10^6 Hz'
-            },
-            'GHz': {
-                '1000000000 Hz'
-                '*10^9 Hz'
-            },
-            'rad/s': {
-                'rad/sec',
-                'rad/second'
-            },
-            '%': {
-                'percent'
-            },
-            'Pa': {
-                'Pascal'
-            },
-            'kPa': {
-                '1000 Pa',
-                '1000 Pascal',
-                '*10^3 Pa'
-            },
-            'MPa': {
-                '1000000 Pa',
-                '1000000 Pascal',
-                '*10^6 Pa'
-            },
-            'GPa': {
-                '1000000000 Pa',
-                '1000000000 Pascal',
-                '*10^9 Pa'
-            },
-            '(dimensionless)': {
-                'dimensionless'
-            }
-        }
-        self.storageModulus = standardizer({'Storage Modulus': namePairs['Storage Modulus']})
-        self.lossModulus = standardizer({'Loss Modulus': namePairs['Loss Modulus']})
-        self.tanDelta = standardizer({'Tan Delta': namePairs['Tan Delta']})
-        self.normalizedStorageModulus = standardizer({'Normalized Storage Modulus': namePairs['Normalized Storage Modulus']})
-        self.normalizedLossModulus = standardizer({'Normalized Loss Modulus': namePairs['Normalized Loss Modulus']})
-        self.normalizedTanDelta = standardizer({'Normalized Tan Delta': namePairs['Normalized Tan Delta']})
-        self.shearStorageModulus = standardizer({'Shear Storage Modulus': namePairs['Shear Storage Modulus']})
-        self.shearLossModulus = standardizer({'Shear Loss Modulus': namePairs['Shear Loss Modulus']})
-        self.temperature = standardizer({'Temperature': namePairs['Temperature']})
-        self.frequency = standardizer({'Frequency': namePairs['Frequency']})
-        self.strain = standardizer({'Strain': namePairs['Strain']})
-        self.temperatureUnit = standardizer({'Celsius': namePairs['Celsius'], 'Kelvin': namePairs['Kelvin']})
-        self.frequencyUnit = standardizer({'Hz': namePairs['Hz'], 'kHz': namePairs['kHz'], 'MHz': namePairs['MHz'], 'GHz': namePairs['GHz'], 'rad/s': namePairs['rad/s']})
-        self.strainUnit = standardizer({'%': namePairs['%']})
-        self.modulusUnit = standardizer({'Pa': namePairs['Pa'], 'kPa': namePairs['kPa'], 'MPa': namePairs['MPa'], 'GPa': namePairs['GPa']})
-        self.dimensionless = standardizer({'(dimensionless)': namePairs['(dimensionless)']})
+        # load json
+        with open(config,'r') as f:
+            fullDict = json.load(f)
+        # load namePairs for stdName and name mapping, X/Y both included
+        nameStdzrs = {}
+        stdNameName = fullDict['stdName-name']
+        for stdName in stdNameName:
+            # create the standardizer object and save it to the stdzrs dict
+            nameStdzrs[stdName] = standardizer({stdName:stdNameName[stdName]})
+        # load namePairs for stdUnit and unit mapping, X/Y both included
+        # an extra layer is needed for unit standardizers because one unit type
+        # could contain multiple stdUnits
+        # example: frequencyUnit: Hz, kHz, MHz, GHz, rad/s
+        unitTypeStdzrs = {}
+        unitTypeStdUnit = fullDict['unitType-stdUnit']
+        stdUnitUnit = fullDict['stdUnit-unit']
+        for unitType in unitTypeStdUnit:
+            # use unitType as the key, find all stdUnit mapped to the unitType,
+            # create a unitType standardizer with all stdUnit-unit pairs
+            allStdUnitUnitPairs = {}
+            for stdUnit in unitTypeStdUnit[unitType]:
+                allStdUnitUnitPairs[stdUnit] = stdUnitUnit[stdUnit]
+            # create the standardizer object and save it to the stdzrs dict
+            unitTypeStdzrs[unitType] = standardizer(allStdUnitUnitPairs)
+        # an example of how xpath-subclass pairs should look like
+        # xpathSubclassPairs = {
+        #     'PROPERTIES/Viscoelastic/DynamicProperties/DynamicPropertyProfile': {
+        #         'xName': {
+        #             frequency standardizer object,
+        #             temperature standardizer object,
+        #             strain standardizer object
+        #         },
+        #         'xUnit': {
+        #             frequencyUnit standardizer object,
+        #             temperatureUnit standardizer object,
+        #             strainUnit standardizer object,
+        #             dimensionless standardizer object
+        #         },
+        #         'yName': {
+        #             storageModulus standardizer object,
+        #             lossModulus standardizer object,
+        #             tanDelta standardizer object,
+        #             normalizedStorageModulus standardizer object,
+        #             normalizedLossModulus standardizer object,
+        #             normalizedTanDelta standardizer object,
+        #             shearStorageModulus standardizer object,
+        #             shearLossModulus standardizer object
+        #         },
+        #         'yUnit': {
+        #             modulusUnit standardizer object,
+        #             dimensionless standardizer object
+        #         }
+        #     }
+        # }
+        xpathSubclassPairs = {}
+        stdNameUnitType = fullDict['stdName-unitType']
+        xpathStdXName = fullDict['xPath-stdXName']
+        xpathStdYName = fullDict['xPath-stdYName']
+        # xpathStdXName and xpathStdYName has the same set of keys
+        for xpath in xpathStdXName:
+            # create the set of standardizers for xName
+            xNameStdzrs = set()
+            for stdXName in xpathStdXName[xpath]:
+                xNameStdzrs.add(nameStdzrs[stdXName])
+            # repeat for yName
+            yNameStdzrs = set()
+            for stdYName in xpathStdYName[xpath]:
+                yNameStdzrs.add(nameStdzrs[stdYName])
+            # create the set of standardizers for xUnit, this set needs to be
+            # created by this route:
+            # xpath-stdXName -> stdName-UnitType -> unitTypeStdzrs[xUnitType]
+            xUnitStdzrs = set()
+            for stdXName in xpathStdXName[xpath]:
+                for xUnitType in stdNameUnitType[stdXName]:
+                    xUnitStdzrs.add(unitTypeStdzrs[xUnitType])
+            # repeat for yUnit
+            yUnitStdzrs = set()
+            for stdYName in xpathStdYName[xpath]:
+                for yUnitType in stdNameUnitType[stdYName]:
+                    yUnitStdzrs.add(unitTypeStdzrs[yUnitType])
+            # save to xpathSubClassPairs
+            xpathSubclassPairs[xpath] = {'xName': xNameStdzrs,
+                                         'xUnit': xUnitStdzrs,
+                                         'yName': yNameStdzrs,
+                                         'yUnit': yUnitStdzrs}
+        # save to the class
+        self.xpathSubclassPairs = xpathSubclassPairs
+        return
+
     # parse function
     def parse(self, xpath, xheader, yheader):
         '''
@@ -345,7 +187,7 @@ class spectraHeaderParser(object):
 if __name__ == '__main__':
     shp = spectraHeaderParser()
     # test case 1
-    xpath = 'PROPERTIES/Viscoelastic/DynamicProperties/DynamicPropertyProfile'
+    xpath = 'PROPERTIES/Viscoelastic/DynamicProperties/DynamicPropertyProfile/data'
     xheader = 'temperature (deg C)'
     yheader = 'storage modulus (Mpa)'
     answer = {
@@ -364,7 +206,7 @@ if __name__ == '__main__':
         'xName':'Frequency',
         'xUnit':'Hz',
         'yName':'Tan Delta',
-        'yUnit':'(dimensionless)'
+        'yUnit':'a.u.'
     }
     print(shp.parse(xpath, xheader, yheader))
     assert shp.parse(xpath, xheader, yheader) == answer, 'Fail test case 2'
